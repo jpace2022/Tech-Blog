14367ce658615880571096621e0b038b49403ff7
James Pace <pacej2022@gmail.com>
2022-11-09 09:58:41 -0800

    update

--- public/js/login.js
+++ public/js/login.js
@@ -25,11 +25,11 @@ const signupFormHandler = async (event) => {
     const password = document.querySelector("#password-signup").value.trim();
 
     if (name && email && password) {
-        const response = await fetch("/api/users", {
-            method: "POST",
-            body: JSON.stringify({ name, email, password }),
-            header: { "Content-type": "application/json"},
-        });
+      const response = await fetch('/api/users', {
+        method: 'POST',
+        body: JSON.stringify({ name, email, password }),
+        headers: { 'Content-Type': 'application/json' },
+      });
         if (response.ok) {
             document.location.replace("/dashboard");
         } else {
